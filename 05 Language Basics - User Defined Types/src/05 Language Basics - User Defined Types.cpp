fe27b0f96a91bef49b3fcb9257c350a31fafd281
jrangel_segula <jrangel@segula.es>
2019-05-09 10:18:40 +0200

    Add Module 05 Language Basics - User Defined Types

---
+++ 05 Language Basics - User Defined Types/src/05 Language Basics - User Defined Types.cpp
@@ -0,0 +1,15 @@
+//============================================================================
+// Name        : 05.cpp
+// Author      : jrangel
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "02 Variables and Fundamental Types" << endl; // prints 02 Variables and Fundamental Types
+	return 0;
+}
