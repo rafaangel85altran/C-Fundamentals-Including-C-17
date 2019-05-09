15f7130a2db9004dcc9dd53f6cf7739376f08997
jrangel_segula <jrangel@segula.es>
2019-05-09 08:45:53 +0200

    Starting the course here in Segula

---
+++ 01 Understanding C++ in context/src/01 Understanding C++ in context.cpp
@@ -0,0 +1,15 @@
+//============================================================================
+// Name        : 01 Understanding C++ in context.cpp
+// Author      : jrangel
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Firs module of the Pluralsight training course, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "!!!Hello Wgfdorld!!!" << endl; // prints !!!Hello World!!!
+	return 0;
+}
---
+++ 02_Variables_and_Fundamental_Types/src/02_Variables_and_Fundamental_Types.cpp
@@ -0,0 +1,15 @@
+//============================================================================
+// Name        : 02_Variables_and_Fundamental_Types.cpp
+// Author      : jrangel
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "02 Variables and Fundamental Types" << endl; // prints 02 Variables and Fundamental Types
+	return 0;
+}
