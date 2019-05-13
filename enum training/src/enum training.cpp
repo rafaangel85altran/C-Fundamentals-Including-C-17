22df39db874ae12ba3df5cb7b2123109318c8d15
jrangel_segula <jrangel@segula.es>
2019-05-13 16:22:45 +0200

    Example of struct

--- enum training/src/enum training.cpp
+++ enum training/src/enum training.cpp
@@ -9,11 +9,26 @@
 #include <iostream>
 using namespace std;
 
-enum carTyre {normal,wet, snow, sports};		//enum permite definir nuestro propio tipo de variables y su scope
+enum mitipodevariable {normal = -1 ,wet, snow, sports};		//enum permite definir nuestro propio tipo de variables y su scope
+
+/*
+ * enum EDriveMode
+{
+   DRIVEMODE_UNKNOWN = -1,
+   DRIVEMODE_NORMAL,
+   DRIVEMODE_RESCUE,
+   DRIVEMODE_REVERSE,
+   DRIVEMODE_TOW,
+   DRIVEMODE_LIMITED_PERFORMANCE,
+   DRIVEMODE_FIX_SPEED_LIMIT,
+   DRIVEMODE_BATT_BACKUP,
+   DRIVEMODE_MAX_NUMBER
+};
+ * */
 
 int main() {
 
-	 carTyre Tipodecoche = snow;
+	mitipodevariable Tipodecoche = sports;	//
 
 	 cout << Tipodecoche;
 
