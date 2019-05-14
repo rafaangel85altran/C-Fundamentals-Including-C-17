a4bd554d42070227f60c8bf95c5e96437b9bd27a
jrangel_segula <jrangel@segula.es>
2019-05-14 16:51:33 +0200

    I don´t understand why os not working

--- ternary_example/src/ternary_example.cpp
+++ ternary_example/src/ternary_example.cpp
@@ -9,11 +9,16 @@
 #include <iostream>
 using namespace std;
 
+
 #define FMAX(a,b) ((a)>(b) ? (a) : (b))
 #define FMIN(a,b) ((a)<(b) ? (a) : (b))
 
+
 int main() {
+
 	int test FMIN(5,10);
 	cout << "Ternary example: " << test << endl; // prints Ternary example
 	return 0;
 }
+
+
